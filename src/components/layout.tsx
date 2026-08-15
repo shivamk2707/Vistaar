4130b30fdd83ab0c01f7bd346c92fff2e5661beb
shivamk2707 <shivamaiuse1@gmail.com>
2026-08-16 00:19:09 +0530

    Build successfull

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -80,7 +80,7 @@ export function SectionHeading({
     visible: { 
       opacity: 1, 
       y: 0,
-      transition: { duration: 0.6, ease: [0.25, 0.46, 0.45, 0.94] }
+      transition: { duration: 0.6, ease: [0.25, 0.46, 0.45, 0.94] as any }
     },
   };
 
